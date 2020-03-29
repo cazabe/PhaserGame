2f554b607a5a4977a3d25ab2631ad936bada2b25
carlos zavala <cazcarlos@hotmail.com>
2020-03-29 17:43:23 -0500

    actualizando el package,json

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const port = 3000; //Specify a port for our web server
+3000; //Specify a port for our web server
 const express = require('express'); //load express with the use of requireJs
 
 const app = express(); //Create an instance of the express library
 
+let port = process.env.PORT;
+
+  if (port == null || port == "") {
+    port = 3000;
+  }
+
 app.use(express.static(__dirname + '/'));//Serving static files
+
 app.listen(port, function() { //Listener for specified port
     console.log("Server running at: http://localhost:" + port)
 });
